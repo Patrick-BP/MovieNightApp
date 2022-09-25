4d755c56a88fd54fe47db910afb923dd349d552a
Patrick <papybp@gmail.com>
2022-09-25 16:59:36 -0500

    bmnmn

--- server/controllers/FavController.js
+++ server/controllers/FavController.js
@@ -5,7 +5,7 @@ const {ObjectId} = require('mongodb')
 
 exports.getAll = async (req, res)=>{
     try{
-       const result = await Favmovie.find({userId:req.params.userId}).populate('Movies');
+       const result = await Favmovie.find({userId:req.params.userId}).populate('movies');
     res.json(result) 
     }catch(e){
         res.json(e)
