8b06acaa6b0906dec55572ee161f3e01719b3002
Patrick <papybp@gmail.com>
2022-09-25 16:31:46 -0500

    mjnmn

--- server/routers/PlaylistRouter.js
+++ server/routers/PlaylistRouter.js
@@ -3,7 +3,7 @@ const playListController = require('../controllers/PlaylistController');
 const route = express.Router();
 
 route.get('/:userId', playListController.getAll);
-route.delete('/:favId', playListController.delById);
+route.delete('/:playlistId', playListController.delById);
 route.post('/add', playListController.save);
 
 module.exports = route 
